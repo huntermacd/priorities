01a9f287c2e4e3135290baa567985518b005073d
Hunter MacDermut <whosebluesanyway@gmail.com>
2016-01-15 13:33:51 -0500

    Implement incrementation function

--- app.js
+++ app.js
@@ -5,22 +5,34 @@ var prioritiesApp = JSON.parse(localStorage.getItem('prioritiesApp')) || {};
 
 $addButton.on('click', function(){
     var userInput = prompt('Enter description:');
+    if (userInput === null || userInput === ''){
+        return;
+    }
     prioritiesApp[userInput] = 0;
-    localStorage.setItem('prioritiesApp', JSON.stringify(prioritiesApp));
+    updateLocalStorage();
     renderItems();
 });
 
-$todosContainer.on('click', 'todo', function(){return;});
+$todosContainer.on('click', '.todo', function(){
+    var key = $(this).find('.description').text();
+    prioritiesApp[key]++;
+    updateLocalStorage();
+    renderItems();
+});
 
 $(document).ready(function(){
     renderItems();
 });
 
+function updateLocalStorage(){
+    localStorage.setItem('prioritiesApp', JSON.stringify(prioritiesApp));
+}
+
 function renderItems(){
-    var parsedPrioritiesObj = JSON.parse(localStorage.getItem('prioritiesApp'));
+    var parsedPrioritiesApp = JSON.parse(localStorage.getItem('prioritiesApp'));
     $todosContainer.empty();
-    for (var item in parsedPrioritiesObj){
-        var todo = '<div class="todo"><p class="description">' + item + '</p><span class="taps">' + parsedPrioritiesObj[item] + '</span></div>';
+    for (var item in parsedPrioritiesApp){
+        var todo = '<div class="todo"><p class="description">' + item + '</p><span class="taps">' + parsedPrioritiesApp[item] + '</span></div>';
         $todosContainer.append(todo);
     }
 }
